402984c0f1679c1c3f247b154f00a83a42162947
sree <sreenadha.k.sheshadri@outlook.com>
2024-05-02 05:27:16 +0000

    added client id

--- a1.provider.tf
+++ a1.provider.tf
@@ -1,5 +1,25 @@
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+tenant_id = var.tenant_id
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
 }
 
 provider "azurerm" {
